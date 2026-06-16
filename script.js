8aa053424c2c562eb8e2ded58ba710a0b9a34bba
Shaine123 <shaineberdida@gmail.com>
2026-06-16 20:07:34 +0800

    added styles and fixed the bugs in gen password feature

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const includeLowerCase = document.getElementById("includeLowercase");
 const includeNumbers = document.getElementById("includeNumbers");
 const includeSymbols = document.getElementById("includeSymbols");
 const genPassBtn = document.getElementById("generatePassword");
+const strengthBar = document.getElementById("meter");
+const strengthType = document.getElementById("strength-type");
 
 const UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
@@ -29,7 +31,7 @@ function generatePassword() {
   } else {
     const newPassword = createNewPassword(rangeBar.value);
 
-    updateUserInterface(newPassword);
+    updateUserInterface(rangeBar.value, newPassword);
   }
 }
 
@@ -49,6 +51,30 @@ function createNewPassword(length) {
   return password;
 }
 
-function updateUserInterface(newPassword) {
+function updateUserInterface(length, newPassword) {
   password.value = newPassword;
+
+  let defPoints = 10;
+
+  if (/[A-Z]/.test(newPassword)) defPoints += 15;
+  if (/[a-z]/.test(newPassword)) defPoints += 15;
+  if (/[0-9]/.test(newPassword)) defPoints += 15;
+  if (/[!@#$%^&*()_?]/.test(newPassword)) defPoints += 15;
+
+  if (length >= 12 && length <= 16) defPoints += 15;
+
+  if (length >= 12 && length <= 24) defPoints += 15;
+
+  if (defPoints < 40) {
+    strengthType.textContent = "Weak";
+    strengthBar.style.backgroundColor = "#e55e5e";
+  } else if (defPoints >= 40 && defPoints <= 75) {
+    strengthType.textContent = "Medium";
+    strengthBar.style.backgroundColor = "#e8e89c";
+  } else if (defPoints >= 75 && defPoints <= 100) {
+    strengthType.textContent = "Strong";
+    strengthBar.style.backgroundColor = "#89e976";
+  }
+
+  strengthBar.style.width = parseInt(defPoints) + "%";
 }
